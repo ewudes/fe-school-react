64dac4cd24ecdabb4c5114761c66fd63366ee3d2
Gunchin Aleksey <gunchin.a@digdes.com>
2022-04-15 21:03:36 +0400

    lecture-2

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,11 +1,25 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Main from '../../pages/main/main';
 import Archive from '../../pages/archive/archive';
+import { AppRoute } from '../../const';
 
 const App = () => {
   return (
-    <Main />
-    // <Archive />
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact>
+          <Main />
+        </Route>
+        <Route path={AppRoute.ARCHIVE} exact component={Archive} />
+        <Route path={AppRoute.EVENT}>
+          {/* Страница добавления/редактирования */}
+        </Route>
+        <Route>
+          <h1>Какой-то текст</h1>
+        </Route>
+      </Switch>
+    </BrowserRouter>
   )
 }
 
--- src/pages/archive/archive.jsx
+++ src/pages/archive/archive.jsx
@@ -2,12 +2,25 @@ import React from 'react';
 import Board from '../../components/board/board';
 import Header from '../../components/header/header';
 import { AppRoute } from '../../const';
+// import { useParams } from 'react-router-dom';
 
-const Archive = () => {
+const Archive = (props) => {
+  // console.log('props', props)
+
+  const { id } = props.match.params;
+  // const { id } = useParams();
+
+  const kek = () => {
+    return id ? `ID есть, вот он - ${id}` : 'Нет ID никакого'
+  }
+
+  console.log('id', id)
+
   return (
     <>
       <Header mode={AppRoute.ARCHIVE}/>
-      <section class="main__wrapper">
+      <section className="main__wrapper">
+        <div>{kek()}</div>
         <Board />
       </section>
     </>
